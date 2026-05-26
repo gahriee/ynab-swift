b9f16e28077e774428a092bb6cff8fb467074837
Edgar Dollentas <edgardollentas04@gmail.com>
2026-05-26 08:18:42 +0800

    first commit

---
+++ ynab/ynabApp.swift
@@ -0,0 +1,17 @@
+//
+//  ynabApp.swift
+//  ynab
+//
+//  Created by Eli on 5/26/26.
+//
+
+import SwiftUI
+
+@main
+struct ynabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
